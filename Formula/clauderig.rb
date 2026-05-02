7e46e831bdf32947836515569b4983c359b43695
VISHAL JANGID <vishaljangid@VISHALs-MacBook-Air.local>
2026-05-02 23:44:23 +0530

    Add clauderig formula

--- Formula/clauderig.rb
+++ Formula/clauderig.rb
@@ -1,14 +1,14 @@
-class Clauderig < Formula 
+class Clauderig < Formula
   desc "My awesome CLI tool"
   homepage "https://github.com/harshit9413/clauderig"
   version "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/mytool_#{version}_macos_arm64.zip"
+      url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/clauderig_#{version}_macos_arm64.zip"
       sha256 "REPLACE_WITH_SHA256"
     else
-      url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/mytool_#{version}_macos_x86_64.zip"
+      url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/clauderig_#{version}_macos_x86_64.zip"
       sha256 "REPLACE_WITH_SHA256"
     end
   end
@@ -18,7 +18,8 @@ class Clauderig < Formula
   def install
     bin.install "clauderig"
   end
-test do
-  system "#{bin}/clauderig", "--help"
-end
+
+  test do
+    assert_match "clauderig", shell_output("#{bin}/clauderig --version")
+  end
 end
