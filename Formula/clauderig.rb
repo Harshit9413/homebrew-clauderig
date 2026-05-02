e6358b96f250671cb1f84301296e07e169ca5a01
VISHAL JANGID <vishaljangid@VISHALs-MacBook-Air.local>
2026-05-02 23:49:18 +0530

    Fix Formula

--- Formula/clauderig.rb
+++ Formula/clauderig.rb
@@ -6,10 +6,10 @@ class Clauderig < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/clauderig_#{version}_macos_arm64.zip"
-      sha256 "REPLACE_WITH_SHA256"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
       url "https://github.com/harshit9413/clauderig/releases/download/v#{version}/clauderig_#{version}_macos_x86_64.zip"
-      sha256 "REPLACE_WITH_SHA256"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
@@ -18,7 +18,8 @@ class Clauderig < Formula
   def install
     bin.install "clauderig"
   end
-test do
-  system "#{bin}/clauderig", "--help"
-end
+
+  test do
+    system "#{bin}/clauderig", "--help"
+  end
 end
